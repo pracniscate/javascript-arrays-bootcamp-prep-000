b3d96d21f1f15b066888612fa7a985a7e214ddf3
Lorena <lorena.giddens@outlook.com>
2018-03-28 20:08:51 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var candyCane =
 
 function addElementToBeginningOfArray(array, element){
   var whatever = ["candy cane", "blissful"];
