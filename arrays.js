caa9153e4c067e221859b24ed080e156cae85ebd
Lorena <lorena.giddens@outlook.com>
2018-03-28 20:09:59 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 var candyCane = "foo";
 
-function addElementToBeginningOfArray(chocolateBars, element){
+function addElementToBeginningOfArray(chocolateBars, candyCane){
   var whatever = ["candy cane", "blissful"];
   array.unshift("organic");
 }
